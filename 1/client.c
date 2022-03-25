8da4db0e17313d2324875e69b7cbb159b3d1d2a4
Ali Ranjbari <aliranjbari86@gmail.com>
2022-03-25 17:50:31 +0430

    first commit

---
+++ 1/client.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+int main(int argc, char* argv[]){
+    int fd;
+    struct sockaddr_in server_address;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_address.sin_family = AF_INET; 
+    server_address.sin_port = htons(80); 
+    server_address.sin_addr.s_addr = inet_addr("4.2.2.4");
+
+    if (connect(fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) { // checking for errors
+        printf("Error in connecting to server\n");
+    }
+
+    return 0;
+}
