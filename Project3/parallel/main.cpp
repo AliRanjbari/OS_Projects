dd8cb575d63f754d1e62c80aa974ff8b09591e7c
Ali Ranjbari <aliranjbari86@gmail.com>
2022-05-22 14:16:48 +0430

    parallel complete.

--- Project3/parallel/main.cpp
+++ Project3/parallel/main.cpp
@@ -56,7 +56,7 @@ typedef struct rgb {
 
 typedef vector<vector<RGB>> PICTURE;
 
-const int N_THREADS = 1;
+const int N_THREADS = 4;
 const int MAX_SIZE = 1080;
 const char OUTPUT_NAME[] = "output.bmp";
 const vector<std::pair<int, int>> DIRS = {{1,0},{1,1},{0,1},{-1,1},
@@ -164,13 +164,22 @@ void *writeOutBmp24(void* id)
         switch (k)
         {
         case 0:
-          fileBuffer[bufferSize - count] = pic[i][j].r;
+          if(picIsOriginal)
+            fileBuffer[bufferSize - count] = pic[i][j].r;
+          else
+            fileBuffer[bufferSize - count] = tempPic[i][j].r;
           break;
         case 1:
-          fileBuffer[bufferSize - count] = pic[i][j].g;
+          if(picIsOriginal)
+            fileBuffer[bufferSize - count] = pic[i][j].g;
+          else
+            fileBuffer[bufferSize - count] = tempPic[i][j].g;
           break;
         case 2:
-          fileBuffer[bufferSize - count] = pic[i][j].b;
+          if(picIsOriginal)
+            fileBuffer[bufferSize - count] = pic[i][j].b;
+          else
+            fileBuffer[bufferSize - count] = tempPic[i][j].b;
           break;
         }
         count++;
@@ -180,6 +189,110 @@ void *writeOutBmp24(void* id)
   pthread_exit(NULL);
 }
 
+RGB median(int x, int y, PICTURE& pic) {
+  int _x, _y, size = 0;
+  vector<DWORD> reds;
+  vector<DWORD> greens;
+  vector<DWORD> blues;
+  for(auto dir : DIRS) {
+    _x = x + dir.first;
+    _y = y + dir.second;
+    if(0 <= _x && _x < rows && 0 <= _y && _y < cols) {
+      reds.push_back(pic[_x][_y].r);
+      greens.push_back(pic[_x][_y].g);
+      blues.push_back(pic[_x][_y].b);
+      size++;
+    }
+  }
+  std::sort(reds.begin(), reds.end());  
+  std::sort(greens.begin(), greens.end());  
+  std::sort(blues.begin(), blues.end());
+
+  return RGB{reds[size/2], greens[size/2], blues[size/2]};
+}
+
+void *horizontalMirrorFilter(void* id) {
+  intptr_t thread_id = (intptr_t)id;
+  int start = rows / N_THREADS * (thread_id);
+  int end = rows / N_THREADS * (thread_id+1);
+  end = (thread_id+1==N_THREADS) ? rows : end;
+
+  for(int x=start; x < end; x++)
+    for(int y=0; y < cols; y++)
+      if(picIsOriginal)
+        tempPic[x][y] = pic[x][cols-y-1];
+      else
+        pic[x][y] = tempPic[x][cols-y-1];
+
+  pthread_exit(NULL);
+}
+
+void *verticalMirrorFilter(void* id) {
+  intptr_t thread_id = (intptr_t)id;
+  int start = rows / N_THREADS * (thread_id);
+  int end = rows / N_THREADS * (thread_id+1);
+  end = (thread_id+1==N_THREADS) ? rows : end;
+
+  for(int x=start; x < end; x++)
+    for(int y=0; y < cols; y++)
+      if(picIsOriginal)
+        tempPic[x][y] = pic[rows-x-1][y];
+      else
+        pic[x][y] = tempPic[rows-x-1][y];
+
+  pthread_exit(NULL);
+}
+
+void *medianFilter(void* id) {
+  intptr_t thread_id = (intptr_t)id;
+  int start = rows / N_THREADS * (thread_id);
+  int end = rows / N_THREADS * (thread_id+1);
+  end = (thread_id+1==N_THREADS) ? rows : end;
+  
+  for(int x=start; x < end; x++)
+    for(int y=0; y < cols; y++)
+      if(picIsOriginal)
+        tempPic[x][y] = median(x, y, pic);
+      else
+        pic[x][y] = median(x, y, tempPic);
+
+  pthread_exit(NULL);
+}
+
+void *inverseColorsFilter(void* id) {
+  intptr_t thread_id = (intptr_t)id;
+  int start = rows / N_THREADS * (thread_id);
+  int end = rows / N_THREADS * (thread_id+1);
+  end = (thread_id+1==N_THREADS) ? rows : end;
+  
+  for(int x=start; x < end; x++)
+    for(int y=0; y < cols; y++) 
+      if(picIsOriginal) {
+        tempPic[x][y].r = 255 - pic[x][y].r;
+        tempPic[x][y].g = 255 - pic[x][y].g;
+        tempPic[x][y].b = 255 - pic[x][y].b;
+      }
+      else {
+        pic[x][y].r = 255 - tempPic[x][y].r;
+        pic[x][y].g = 255 - tempPic[x][y].g;
+        pic[x][y].b = 255 - tempPic[x][y].b;
+      }
+    
+}
+
+void addPlusSign(PICTURE& input) {
+  for(int x=0; x < rows; x++) {
+    input[x][cols/2+1] = RGB{255, 255, 255};
+    input[x][cols/2] = RGB{255, 255, 255};
+    input[x][cols/2-1] = RGB{255, 255, 255};
+  }
+  for(int y=0; y < cols; y++) {
+    input[rows/2+1][y] = RGB{255, 255, 255};
+    input[rows/2][y] = RGB{255, 255, 255};
+    input[rows/2-1][y] = RGB{255, 255, 255};
+  }
+}
+
 
 auto current_time() {
   return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
@@ -187,6 +300,9 @@ auto current_time() {
 
 int main(int argc, char *argv[])
 {
+
+  auto timeStart = current_time();
+
   char *fileName = argv[1];
   if (!fillAndAllocate(fileBuffer, fileName, rows, cols, bufferSize))
   {
@@ -195,6 +311,7 @@ int main(int argc, char *argv[])
   }
 
   pthread_t threads[N_THREADS];
+  cout << "**Program is running with " << N_THREADS << " threads**\n";
 
 
   // ..........................................
@@ -206,6 +323,59 @@ int main(int argc, char *argv[])
   auto t2 = current_time();
   cout << "Read from file: Done in " << t2 - t1 << " ms\n";
 
+  cout << "Filters: \n";
+  // ..........................................
+  t1 = current_time();
+  for(int i=0; i < N_THREADS; i++)
+    pthread_create(&threads[i], NULL, horizontalMirrorFilter, (void*)i);
+  for(int i=0; i<N_THREADS; i++)
+    pthread_join(threads[i], NULL);
+  picIsOriginal = !picIsOriginal;
+  t2 = current_time();
+  cout << "--> Horizontal Mirror: Done in " << t2 - t1 << " ms\n";
+
+  // ..........................................
+  t1 = current_time();
+  for(int i=0; i < N_THREADS; i++)
+    pthread_create(&threads[i], NULL, verticalMirrorFilter, (void*)i);
+  for(int i=0; i<N_THREADS; i++)
+    pthread_join(threads[i], NULL);
+  picIsOriginal = !picIsOriginal;
+  t2 = current_time();
+  cout << "--> Vertical Mirror: Done in " << t2 - t1 << " ms\n";
+
+
+  // ..........................................
+  t1 = current_time();
+  for(int i=0; i < N_THREADS; i++)
+    pthread_create(&threads[i], NULL, medianFilter, (void*)i);
+  for(int i=0; i<N_THREADS; i++)
+    pthread_join(threads[i], NULL);
+  picIsOriginal = !picIsOriginal;
+  t2 = current_time();
+  cout << "--> Median : Done in " << t2 - t1 << " ms\n";
+
+  // ..........................................
+  t1 = current_time();
+  for(int i=0; i < N_THREADS; i++)
+    pthread_create(&threads[i], NULL, inverseColorsFilter, (void*)i);
+  for(int i=0; i<N_THREADS; i++)
+    pthread_join(threads[i], NULL);
+  picIsOriginal = !picIsOriginal;
+  t2 = current_time();
+  cout << "--> Inverse Colors: Done in " << t2 - t1 << " ms\n";
+
+
+  // ..........................................
+  t1 = current_time();
+  if(picIsOriginal)
+    addPlusSign(pic);
+  else
+    addPlusSign(tempPic);
+  t2 = current_time();
+  cout << "--> Added Plus sign: Done in " << t2 - t1 << " ms\n";
+
+
   // ..........................................
   t1 = current_time();
   for(int i=0; i < N_THREADS; i++)
@@ -215,6 +385,10 @@ int main(int argc, char *argv[])
   t2 = current_time();
   cout << "Wrote bmp: Done in " << t2 - t1 << " ms\n";
 
+
+  auto timeEnd = current_time();
+  cout << "------------------------------\n";
+  cout << "Execution Time: " << timeEnd - timeStart << " ms\n";
 
   return 0;
 }
